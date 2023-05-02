865dda27c91560e3810e24a25589aa936901ada1
Vaibhav Totla <vtotla.tech@gmail.com>
2023-05-02 06:36:01 +0530

    code upload

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,15 @@
+<!-- Footer -->
+<div id="footer">
+          <div class="left"> <a href="#">Home</a> <span>|</span> <a href="#">Support</a> <span>|</span> <a href="#">My Account</a> <span>|</span> <a href="#">The Store</a> <span>|</span> <a href="#">Contact</a> </div>
+          <div class="right"> &copy; Sitename.com. Design by <a href="http://chocotemplates.com">ChocoTemplates.com</a> </div>
+        </div>
+        <!-- End Footer -->
+      </div>
+      <!-- End Container -->
+    </div>
+    <!-- End Content -->
+  </div>
+</div>
+<!-- End Main -->
+</body>
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Wing the Air</title>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css" media="all" />
+<!--[if lte IE 6]><style type="text/css" media="screen">.tabbed { height:420px; }</style><![endif]-->
+<script src="{{asset('jquery-1.4.1.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('jquery.jcarousel.pack.js')}}" type="text/javascript"></script>
+<script src="{{asset('jquery.slide.js')}}" type="text/javascript"></script>
+<script src="{{asset('jquery-func.js')}}" type="text/javascript"></script>
+</head>
+<body>
